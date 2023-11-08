0be9c81338dc4da8a44a5d0ac0d6fb43e088ba1c
querenneves <querensantos1@hotmail.com>
2023-11-07 21:18:28 -0300

    Fortalecendo a base

---
+++ app.js
@@ -0,0 +1,11 @@
+const carousel = document.getElementById("carousel");
+const images = document.querySelectorAll(".carousel-slide img");
+let currentImageIndex = 0;
+
+function rotateImages() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    const translateValue = -currentImageIndex * 300 + "px";
+    carousel.style.transform = `translateX(${translateValue})`;
+}
+
+setInterval(rotateImages, 3000); // Muda a cada 3 segundos (3000 milissegundos)
